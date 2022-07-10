5c05131a8524b089d15e7c009edb4efdad3c11dd
leejunho <leejun0718@naver.com>
2022-07-10 22:32:22 +0900

    게시판 페이징 추가 및 Junit 테스트

--- src/main/java/com/fastcampus/ch4/dao/BoardDao.java
+++ src/main/java/com/fastcampus/ch4/dao/BoardDao.java
@@ -1,8 +1,13 @@
 package com.fastcampus.ch4.dao;
 
+
 import com.fastcampus.ch4.domain.BoardDto;
+
+
 
 public interface BoardDao {
 	BoardDto select(int bno) throws Exception;
+	
+	int update(BoardDto boardDto) throws Exception;
 	
 }
--- src/main/java/com/fastcampus/ch4/dao/BoardDaoImpl.java
+++ src/main/java/com/fastcampus/ch4/dao/BoardDaoImpl.java
@@ -2,18 +2,27 @@ package com.fastcampus.ch4.dao;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.fastcampus.ch4.domain.BoardDto;
 
+
+@Repository
 public class BoardDaoImpl implements BoardDao {
 	@Autowired
 	SqlSession session;
 	String namespace = "com.fastcampus.ch4.dao.BoardMapper.";
 	
 	
-   BoardDto select(int bno)throws Exception{
+   public BoardDto select(int bno)throws Exception{
+	   
 	return session.selectOne(namespace+"select",bno);
 	
+   }
+   
+   public int update(BoardDto boardDto) throws Exception{
+	   
+	   return session.update(namespace+"update",boardDto);
    }
 	
 
